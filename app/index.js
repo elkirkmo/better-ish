49ea9a25bbde66daf2f5ced20c09049db4f64367
Chris Kirkham <me@chriskirkham.com>
2018-12-09 13:17:00 -0700

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,29 @@
+import clock from "clock";
+import document from "document";
+import { preferences } from "user-settings";
+import * as util from "../common/utils";
+
+// Update the clock every minute
+clock.granularity = "minutes";
+const bg = document.getElementById('bg');
+
+
+// Get a handle on the <text> element
+const hour = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const exact = document.getElementById("exact");
+const dateLine = document.getElementById("date");
+// Update the <text> element every tick with the current time
+clock.ontick = (evt) => {
+  let today = evt.date;
+  let hours = today.getHours();
+  let mins = util.zeroPad(today.getMinutes());
+  hour.text = `${mins > 54 ? util.convertHourToText(hours + 1) : util.convertHourToText(hours)}`;
+  minutes.text = `${util.convertMinuteToText(mins) ? util.convertMinuteToText(mins) + '~ish' : 'ish'}`;
+  exact.text = `${hours}:${mins}`;
+  dateLine.text = `${util.monthName(today.getMonth())} ${today.getDate()}, ${today.getFullYear()}`;
+}
+
+bg.onclick = () => {
+  util.showHide([exact, hour, minutes, dateLine]);
+}
---
+++ common/utils.js
@@ -0,0 +1,38 @@
+// Add zero in front of numbers < 10
+export function zeroPad(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+export function convertHourToText(j) {
+  const k = j > 12 ? j - 12 : j;
+  console.log(j);
+  const a = ['twelve', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'];
+  return a[k];
+}
+
+export function convertMinuteToText(l) {
+    const rounded = Math.round(l / 10) * 10;
+    const b = {
+    10: 'ten',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+  } 
+   return b[rounded];
+}
+
+export function monthName(num) {
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+  return monthNames[num];
+}
+
+
+export function showHide(arr) {
+  arr.forEach(v => v.style.display === "none" ? v.style.display = "inline" : v.style.display = "none");
+}
