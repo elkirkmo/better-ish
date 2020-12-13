c373c0d21486b52dc452d7a023ba1f1e699f4690
Chris Kirkham <me@chriskirkham.com>
2020-12-13 08:13:47 -0700

    feat(): update to sdk 4.2

--- common/utils.js
+++ common/utils.js
@@ -1,9 +1,6 @@
 // Add zero in front of numbers < 10
 export function zeroPad(i) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  return i;
+  return i < 10 ? "0" + i.toString() : i.toString();
 }
 
 export function convertHourToText(j) {
